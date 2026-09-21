7be4eabb278eae0256cf32273fe2ef7bfea1bbc6
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-21 16:22:29 -0300

    V1.0.5

--- notas.php
+++ notas.php
@@ -19,7 +19,7 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
     $n4 = $_POST["n4"];
     $n5 = $_POST["n5"];
 
-   $media = (($nota1*2)+($nota2*3)+($nota3*1)+($nota4*1)+($nota5*3))/10;
+   $media = (($n1*2)+($n2*3)+($n3*1)+($n4*1)+($n5*3))/10;
 
    if($media >= 7){
     $res = "Aprovado!!";
