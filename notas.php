40ef2100a2f08e27d2e3a47a9f07f5addb480119
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-21 16:14:55 -0300

    V1.0.4

--- notas.php
+++ notas.php
@@ -2,6 +2,7 @@
 $nome = "";
 $idade = "";
 $res ="";
+$media = 0;
 $n1 = 0;
 $n2 = 0;
 $n3 = 0;
@@ -17,11 +18,16 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
     $n3 = $_POST["n3"];
     $n4 = $_POST["n4"];
     $n5 = $_POST["n5"];
-    
-    if (($nome || $idade || $n1 || $n2 || $n3 || $n4|| $n5 )== ""){
 
-        $res = "preenche todos os campos";
-    }
+   $media = (($nota1*2)+($nota2*2)+($nota3*2)+($nota4*2)+($nota5*2))/10;
+
+   if($media >= 7){
+    $res = "Aprovado!!";
+   } elseif($media >=5 && $media <7){
+    $res = "Recuperação";
+   } else{
+    $res = "Reprovado";
+   }
 
 }
 ?>
@@ -40,19 +46,22 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
         <div class="inputs">
             <form method="POST">
                 <input type="text" id="nome" name="nome" placeholder="Digite seu nome">
-                <input type="number" id="idade" name="idade" placeholder="digite seu idade">
-                <input type="number" id="n1" name="n1" placeholder="digite o nota 1">
-                <input type="number" id="n2" name="n2" placeholder="digite o nota 2">
-                <input type="number" id="n3" name="n3" placeholder="digite o nota 3">
-                <input type="number" id="n4" name="n4" placeholder="digite o nota 4">
-                <input type="number" id="n5" name="n5" placeholder="digite o nota 5">
+                <input type="number" min="0" max="99" id="idade" name="idade" placeholder="digite seu idade">
+                <input type="number" min="0" max="10" id="n1" name="n1" placeholder="digite o nota 1">
+                <input type="number" min="0" max="10" id="n2" name="n2" placeholder="digite o nota 2">
+                <input type="number" min="0" max="10" id="n3" name="n3" placeholder="digite o nota 3">
+                <input type="number" min="0" max="10" id="n4" name="n4" placeholder="digite o nota 4">
+                <input type="number" min="0" max="10" id="n5" name="n5" placeholder="digite o nota 5">
                 <button type="submit">Enviar</button>
             </form>
         </div>
         <div class="resultados">
 
-        <?php if($res == "")  {?>
+        <?php if($res != "")  {?>
             <h2><?= $nome ?></h2>
+            <h2><?= $idade ?></h2>
+            <strong>Média:</strong><?= $media ?>
+            <h3><?= $res ?></h3>
 
             <?php } ?>
         </div>
