dc5c5aa5dca652ba16892705acafbe343aab186a
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-22 16:23:05 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -4,7 +4,6 @@ $idade = "";
 $res = "";
 $media = 0;
 $frequencia = 0;
-$calcul=0;
 $n1 = 0;
 $n2 = 0;
 $n3 = 0;
@@ -29,10 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } elseif ($media >= 5 && $media < 7 && $frequencia >= 75) {
         $res = "Recuperação";
     } elseif ($media < 5 && $frequencia >= 75) {
-        $calcul = 7-$media;
         $res = "Reprovado por nota";
-        echo"falta $calcul para passar";
-
     } elseif ($media > 7 && $frequencia < 75) {
         $res = "Reprovado por frequência!!!!!!";
     } else {
@@ -57,21 +53,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <main class="container">
         <div class="inputs">
             <form method="POST">
-                <label for="nome">Nome</label>
+                <label for="nome"><strong>Nome</strong></label>
                 <input type="text" id="nome" name="nome" placeholder="Digite o nome do aluno">
-                <label for="idade">Idade</label>
+                <label for="idade"><strong>Idade</strong></label>
                 <input type="number" min="0" max="99" id="idade" name="idade" placeholder="digite a idade do aluno">
-                <label for="frequencia">Fequência</label>
+                <label for="frequencia"><strong>Fequência</strong></label>
                 <input type="number" min="0" max="100" id="frequencia" name="frequencia" placeholder="digite a porcentagem de frequencia do aluno" />
-                <label for="n1">Nota1 (peso 2)</label>
+                <label for="n1"> <strong>Nota1</strong> (peso 2)</label>
                 <input type="number" min="0" max="10" id="n1" name="n1" placeholder="digite o nota 1">
-                <label for="n2">Nota2 (peso 3)</label>
+                <label for="n2"><strong>Nota2</strong> (peso 3)</label>
                 <input type="number" min="0" max="10" id="n2" name="n2" placeholder="digite o nota 2">
-                <label for="n3">Nota3 (peso 1)</label>
+                <label for="n3"><strong>Nota3</strong> (peso 1)</label>
                 <input type="number" min="0" max="10" id="n3" name="n3" placeholder="digite o nota 3">
-                <label for="n4">Nota4 (peso 1)</label>
+                <label for="n4"><strong>Nota4</strong> (peso 1)</label>
                 <input type="number" min="0" max="10" id="n4" name="n4" placeholder="digite o nota 4">
-                <label for="n5">Nota5 (peso 3)</label>
+                <label for="n5"><strong>Nota5</strong> (peso 3)</label>
                 <input type="number" min="0" max="10" id="n5" name="n5" placeholder="digite o nota 5">
                 <button type="submit">Enviar</button>
                 <a href="index.php">voltar</a>
