32f5a22f59a79a7c796488bf265940637c407843
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-22 14:38:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 <a href="idade.php">Verifiador de idade</a>
                 <a href="notas.php">Verificador de notas</a>
                 <a href="receber.php">receber notas</a>
-                <a href="https/google.com">ir para google</a>
+                <a href="https://www.google.com/?hl=pt_BR">ir para google</a>
             </ul>
         </div>
 
